0f9c48a63d55b7d8ea8841f6289b7682fe1cec63
GrischK <grischka.gorski@gmail.com>
2022-04-15 14:48:15 +0200

    adding random button

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
 import "./App.css";
 import { useState } from "react";
+import axios from "axios";
 import DisplayEmployee from "./Components/DisplayEmployee";
 
-const sampleEmployee = {
 
-  gender: 'male',
+
+const sampleEmployee = {
+  gender: "male",
 
   name: {
-
-    first: 'Charlie',
-
-    last: 'Thompson',
-
+    first: "Charlie",
+    last: "Thompson",
   },
 
   location: {
-
     street: {
-
       number: 761,
-
-      name: 'Tay Street',
-
+      name: "Tay Street",
     },
 
-    city: 'Timaru',
-
+    city: "Timaru",
     postcode: 76111,
-
   },
 
-  email: 'charlie.thompson@example.com',
+  email: "charlie.thompson@example.com",
 
   picture: {
-
-    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
-
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
   },
-
 };
-function App() {
-  return <DisplayEmployee employee={sampleEmployee} />;
-}
 
-export default App;
+export default function App() {
+  const getEmployee = () => {
+    axios
+    .get('https://randomuser.me/api?nat=en')
+    .then((response) => response.data)
+    .then((data) => {
+      setEmployee(data.results[0]);
+  });
+  };
+  
+  const [employee, setEmployee] = useState(sampleEmployee);
+
+  return (
+    <div className="app">
+      <DisplayEmployee employee={employee} />
+      <button type="button" onClick={getEmployee}>
+        Get new employee
+      </button>
+    </div>
+  );
+};
